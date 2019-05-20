9b814aa0419ff82ca53785bada63bad1bd6b53ad
paucar97 <c.paucarpoma@pucp.pe>
2019-05-20 16:56:43 -0500

    se agrego las credenciales de la BD de la VM

--- backend/app/commons/db.py
+++ backend/app/commons/db.py
@@ -6,16 +6,23 @@ from io import StringIO
 
 
 def getSessionServer():
-    user='admin'
-    ssh_user = 'ubuntu'
-    passw= 'Telefonica#2019'
-    dbName = 'test'
-    localhost = 'ec2-18-222-229-189.us-east-2.compute.amazonaws.com'
+    user='root'
+    ssh_user = 'inf245'
+    passwssh = "6CmINL2eRPo%baH"
+    passw= 'GiME4OI9'
+    dbName = 'florestack'
+    localhost = '127.0.0.1'
     port = 3307
-    host = "ec2-18-222-229-189.us-east-2.compute.amazonaws.com"
-
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(user,passw,host,port,dbName)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:holi123@localhost:3306/florestack'
+    host = "200.16.7.185"
+    tunnel = sshtunnel.SSHTunnelForwarder(
+            (host,22),
+            ssh_password = passwssh,
+            ssh_username=ssh_user,
+            remote_bind_address=(localhost, 3306)
+    )
+    tunnel.start()
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}:{}/{}'.format(user,passw,localhost,tunnel.local_bind_port,dbName)
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:holi123@localhost:3306/florestack'
     app.config['SQLALCHEMY_POOL_SIZE'] = 5
     app.config['SQLALCHEMY_POOL_TIMEOUT'] = 30
     app.config['SQLALCHEMY_POOL_RECYCLE']=31
