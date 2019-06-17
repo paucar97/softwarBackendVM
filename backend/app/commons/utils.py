6f559ccf933ab443ee830212a4a2f0951d995423
paucar97 <c.paucarpoma@pucp.pe>
2019-06-17 11:19:18 -0500

    agrego la funcion de enviar correos

--- backend/app/commons/utils.py
+++ backend/app/commons/utils.py
@@ -1,5 +1,19 @@
+import smtplib
+
 def convertDatetime(tiempo):
     tiempo = tiempo.__str__()
     tiempo = tiempo.replace("T"," ")
     tiempo = tiempo[:-6]
     return tiempo
+
+correoSistemaSec = "florestacksistemasec2@gmail.com"
+contrasenaSistemaSec = "florestack123"
+
+def envioCorreo(destinatario,asunto,mensage):
+    me = 'Subject: {}\n\n{}'.format(asunto,mensage)
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login(correoSistemaSec,contrasenaSistemaSec)
+    server.sendmail(correoSistemaSec,destinatario,me)
+    server.quit()
+    return
