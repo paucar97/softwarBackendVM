157a46a3d0e032e6730eb4a96d0b9ff8ea4ef7a0
Daniel Alpiste <daniel.alpiste@pucp.pe>
2019-06-16 21:42:06 -0500

    cambios a base de datos para el registro de esfuerzo

--- backend/app/models/categoria_respuesta_alumno.py
+++ backend/app/models/categoria_respuesta_alumno.py
@@ -7,7 +7,7 @@ class Categoria_respuesta_alumno(db.Model):
     __tablename__= 'categoria_respuesta_alumno'
     
     categoria = db.relationship(Categoria,backref = __tablename__,lazy=True)
-    id_categoria = db.Column('ID_CATEGORIA',db.Integer,primary_key=True)
+    id_categoria = db.Column('ID_CATEGORIA',db.ForeignKey(Categoria.id_categoria),primary_key=True)
     
     alumno = db.relationship(Usuario,backref = __tablename__,lazy=True)
     id_alumno = db.Column('ID_ALUMNO',db.ForeignKey(Usuario.id_usuario),primary_key=True)
