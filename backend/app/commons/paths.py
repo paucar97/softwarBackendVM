a17e13fc6b8dd597885d1fd7b478e7c0e2567f75
paucar97 <c.paucarpoma@pucp.pe>
2019-06-18 20:04:26 -0500

    carga masiva

--- backend/app/commons/paths.py
+++ backend/app/commons/paths.py
@@ -1,3 +1,4 @@
 # path de prueba para guardar los archivos
 pathTest = "app/documents/test/"
 urlDecarga = "http://200.16.7.185:8000/test/"
+pathCargaMasivaAlumnoHorario = "app/documents/cargaMasiva/alumno_horario"
