8c4a28a447e05569c6bbf8790d0ad80d53421f7e
paucar97 <c.paucarpoma@pucp.pe>
2019-06-24 14:29:39 -0500

    se activo un semestre

--- backend/app/models/semestre.py
+++ backend/app/models/semestre.py
@@ -24,5 +24,16 @@ class Semestre(db.Model):
     def getAllNoActivos(self):
         s = Semestre.query.filter_by(flg_activo = 0)
         return s
+    @classmethod
     def getAll(self):
         return Semestre.query.filter_by(flg_activo=0).all()
+
+    @classmethod
+    def activar(self,idSemestre):
+        aux = Semestre.query.filter_by(flg_activo =1).first()
+        if aux != None:
+            aux.flg_activo = 0
+        sem = Semestre.query.filter_by(id_semestre = idSemestre).first()
+        sem.flg_activo = 1
+        db.session.commit()
+        return
