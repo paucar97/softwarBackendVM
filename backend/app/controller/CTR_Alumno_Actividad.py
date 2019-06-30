217f9eecc157eba459cc2f541a66941a0e6234f5
paucar97 <c.paucarpoma@pucp.pe>
2019-06-30 11:29:04 -0500

    arreglo en sumaCoevaluac

--- backend/app/controller/CTR_Alumno_Actividad.py
+++ backend/app/controller/CTR_Alumno_Actividad.py
@@ -854,7 +854,7 @@ def sumaCoevaluacion(idGrupo, idActividad):
         sumaDesempeno = 0
         sumaCriterio = 0
         sumaCheck = 0
-        aspectosAlumno = db.session.query(aspectos.c.ID_ASPECTO, aspectos.C.TIPO_CLASIFICACION,Alumno_nota_aspecto.nota).join(Alumno_nota_aspecto, and_(aspectos.C.ID_ASPECTO == Alumno_nota_aspecto.id_aspecto, Alumno_nota_aspecto.id_alumno == miembro.id_alumno)).all()
+        aspectosAlumno = db.session.query(aspectos.c.ID_ASPECTO, aspectos.c.TIPO_CLASIFICACION,Alumno_nota_aspecto.nota).join(Alumno_nota_aspecto, and_(aspectos.C.ID_ASPECTO == Alumno_nota_aspecto.id_aspecto, Alumno_nota_aspecto.id_alumno == miembro.id_alumno)).all()
         for aspecto in aspectosAlumno:
             if aspecto.tipo_clasificacion == 1:
                 sumaDesempeno = sumaDesempeno + aspecto.nota
