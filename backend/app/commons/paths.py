3541353c82c0533b327226d913dfefe9aedac6fe
paucar97 <c.paucarpoma@pucp.pe>
2019-07-12 16:45:16 -0500

    falto un /

--- backend/app/commons/paths.py
+++ backend/app/commons/paths.py
@@ -1,5 +1,5 @@
 # path de prueba para guardar los archivos
 pathTest = "app/documents/test/"
 urlDecarga = "http://200.16.7.185:8000/test/"
-pathCargaMasivaAlumnoHorario = "app/documents/cargaMasiva/alumno_horario"
-pathCargaMasivaCursoHorario = "app/documents/cargaMasiva/curso_horario"
+pathCargaMasivaAlumnoHorario = "app/documents/cargaMasiva/alumno_horario/"
+pathCargaMasivaCursoHorario = "app/documents/cargaMasiva/curso_horario/"
